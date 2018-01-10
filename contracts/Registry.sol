219dc13ddf15063bcfaef679cfc4a43539166069
Arunas Vaitkus <arunas.vaitkus@mapof.ag>
2018-01-10 20:56:55 +0000

    build simple structures

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -8,11 +8,26 @@ contract Registry is RBAC {
   address public creator;
   address public registrant;
   string public vin;
-  mapping(string => address) VinForAddress;
+  mapping(string => VehicleDetails) VinForAddress;
 
     function Registry() public {
       creator = msg.sender;
     }
+
+
+  // ===== Enums  =====
+  // VehicleOwnership enumerator used to define a state
+  // of it's ownership. Details about vehicle can be already
+  // added to the database however vehicle itself is not claimed.
+  enum VehicleOwnership { Claimed, Unclaimed, Unkown }
+
+  // ===== Structures =====
+  struct VehicleDetails {
+    address vehicleAddress;
+    address registrantAddress;
+    VehicleOwnership state;
+  }
+
 
   // ===== Events =====
   event ClaimedVehicleRecord(
@@ -27,12 +42,18 @@ contract Registry is RBAC {
   onlyAdmin
   {
     address vehicle = new Vehicle(_strVin, _registrant);
-    VinForAddress[_strVin] = vehicle;
+    VinForAddress[_strVin].vehicleAddress = vehicle;
+    VinForAddress[_strVin].registrantAddress = _registrant;
+    VinForAddress[_strVin].state = VehicleOwnership.Claimed;
     addVehicleClaim(_registrant, _strVin, vehicle);
   }
 
   function addVehicleClaim(address _registrant, string _strVin, address _vehicleContract) public {
     ClaimedVehicleRecord(_registrant, Utilities.convertStringToBytes32(_strVin), _strVin, _vehicleContract);
+  }
+
+  function getMapping(string _vin) public constant returns (address add) {
+    return VinForAddress[_vin].vehicleAddress;
   }
 
 
